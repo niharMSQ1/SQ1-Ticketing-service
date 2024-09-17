f3d150e4eeffdc98e6c9d969605cefeb9628dd4a
niharMSQ1 <155614287+niharMSQ1@users.noreply.github.com>
2024-09-17 12:00:28 +0530

    Update scheduler.py

--- ticektingOperationsApp/scheduler.py
+++ ticektingOperationsApp/scheduler.py
@@ -3712,8 +3712,8 @@ def start_scheduler():
     now = datetime.now(pytz.UTC)
     today = now.date()
 
-    start_hour = 7
-    start_minute = 25
+    start_hour = 6
+    start_minute = 35
 
     scheduler.add_job(freshservice_call_create_ticket, CronTrigger(hour=start_hour, minute=start_minute, day_of_week='*', start_date=datetime.combine(today, time(start_hour, start_minute), tzinfo=pytz.UTC)))
     scheduler.add_job(jira_call_create_ticket, CronTrigger(hour=start_hour, minute=start_minute + 3, day_of_week='*', start_date=datetime.combine(today, time(start_hour, start_minute + 3), tzinfo=pytz.UTC)))
