76f9f719becc32be8a09e8b724ed65f37489271b
niharMohantyDev <mnihar198@.com>
2024-09-30 14:55:04 +0530

    added new time for scheduler

--- ticektingOperationsApp/scheduler.py
+++ ticektingOperationsApp/scheduler.py
@@ -3981,7 +3981,7 @@ def changeVulnerabilityStatusForTrello():
 def start_scheduler():
     scheduler = BackgroundScheduler(timezone=pytz.UTC)
 
-    start_time = datetime.now(pytz.UTC).replace(hour=8, minute=30, second=0, microsecond=0)
+    start_time = datetime.now(pytz.UTC).replace(hour=10, minute=15, second=0, microsecond=0)
 
     scheduler.add_job(freshservice_call_create_ticket, CronTrigger(hour=start_time.hour, minute=start_time.minute, day_of_week='*', start_date=start_time))
     scheduler.add_job(jira_call_create_ticket, CronTrigger(hour=start_time.hour, minute=(start_time.minute + 3) % 60, day_of_week='*', start_date=start_time))
